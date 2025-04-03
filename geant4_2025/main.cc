cdd8577e663017a217eefe4101445430b743cbef
muslu <musluoglu.mert10@gmail.com>
2025-04-03 21:13:58 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -2,10 +2,9 @@
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
-#include "G4SystemOfUnits.hh"
 
-#include "PhysicsList.hh"
 #include "DetectorConstruction.hh"
+#include "PhysicsList.hh"
 #include "PrimaryGeneratorAction.hh"
 #include "RunAction.hh"
 #include "EventAction.hh"
@@ -14,7 +13,7 @@ int main(int argc, char** argv) {
     // 1. RunManager oluştur
     G4RunManager* runManager = new G4RunManager();
 
-    // 2. Zorunlu başlatma sırası
+    // 2. Zorunlu başlatma
     runManager->SetUserInitialization(new DetectorConstruction());
     runManager->SetUserInitialization(new PhysicsList());
 
@@ -26,18 +25,15 @@ int main(int argc, char** argv) {
     // 4. Görselleştirme
     G4VisManager* visManager = new G4VisExecutive();
     visManager->Initialize();
-    
-    // 5. UI (GUI veya batch mod)
+
+    // 5. UI
     G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-    G4UImanager* UI = G4UImanager::GetUIpointer();
+    G4UImanager::GetUIpointer()->ApplyCommand("/control/execute macros/init_vis.mac");
     
-    // Otomatik 3D görünüm
-    UI->ApplyCommand("/control/execute macros/init_vis.mac");
-    UI->ApplyCommand("/run/initialize");
-    
-    // 6. Simülasyonu başlat
+    // 6. Simülasyon
+    runManager->Initialize();
     ui->SessionStart();
-    
+
     delete ui;
     delete visManager;
     delete runManager;
