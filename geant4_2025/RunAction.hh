61460fb4b099354368cb5ee62cf7edeaa25bca24
muslu <musluoglu.mert10@gmail.com>
2025-04-03 17:49:24 +0300

    Update RunAction.hh

--- geant4_2025/RunAction.hh
+++ geant4_2025/RunAction.hh
@@ -7,8 +7,8 @@
 class RunAction : public G4UserRunAction {
 public:
     RunAction();
-    virtual ~RunAction();
-
+    virtual ~RunAction();  // Düzeltme: Virtual destructor eklendi
+    
     virtual void BeginOfRunAction(const G4Run*);
     virtual void EndOfRunAction(const G4Run*);
 };
