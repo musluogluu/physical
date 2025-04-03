74a566936427f5d01cbaaf263d2bed195f24a08f
muslu <musluoglu.mert10@gmail.com>
2025-04-03 17:21:44 +0300

    Update DetectorConstruction.cc

--- geant4_2025/DetectorConstruction.cc
+++ geant4_2025/DetectorConstruction.cc
@@ -1,18 +1,24 @@
 #include "DetectorConstruction.hh"
 #include "G4NistManager.hh"
 #include "G4Tubs.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
 
-DetectorConstruction::DetectorConstruction() 
-  : fDiameter(10*cm), fHeight(10*cm) {}
+DetectorConstruction::DetectorConstruction()
+: fDiameter(10*cm), fHeight(10*cm), fMaterial(nullptr) {}
 
 G4VPhysicalVolume* DetectorConstruction::Construct() {
-  G4Material* tungsten = G4NistManager::Instance()->FindOrBuildMaterial("G4_W");
-  G4Tubs* solidTarget = new G4Tubs("Target", 0, fDiameter/2, fHeight/2, 0, 360*deg);
-  G4LogicalVolume* logicTarget = new G4LogicalVolume(solidTarget, tungsten, "Target");
-  return new G4PVPlacement(0, G4ThreeVector(), logicTarget, "Target", 0, false, 0);
+    G4Tubs* solidTarget = new G4Tubs("Target", 0, fDiameter/2, fHeight/2, 0, 360*deg);
+    G4LogicalVolume* logicTarget = new G4LogicalVolume(solidTarget, fMaterial, "Target");
+    return new G4PVPlacement(0, G4ThreeVector(), logicTarget, "Target", 0, false, 0);
 }
 
-void DetectorConstruction::SetSize(G4double diameter, G4double height) {
-  fDiameter = diameter;
-  fHeight = height;
+void DetectorConstruction::SetMaterial(const G4String& materialName) {
+    fMaterial = G4NistManager::Instance()->FindOrBuildMaterial(materialName);
+}
+
+void DetectorConstruction::SetDimensions(G4double diameter, G4double height) {
+    fDiameter = diameter;
+    fHeight = height;
 }
