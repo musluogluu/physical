16c7c2934b22d1552c112bd174c592dd8b009ea1
muslu <musluoglu.mert10@gmail.com>
2025-04-03 17:49:57 +0300

    Update EventAction.hh

--- geant4_2025/EventAction.hh
+++ geant4_2025/EventAction.hh
@@ -7,8 +7,8 @@
 class EventAction : public G4UserEventAction {
 public:
     EventAction();
-    virtual ~EventAction();
-
+    virtual ~EventAction();  // Düzeltme: Virtual destructor eklendi
+    
     virtual void BeginOfEventAction(const G4Event*);
     virtual void EndOfEventAction(const G4Event*);
 };
