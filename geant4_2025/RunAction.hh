98b2453316b07ccb643670d83391884e30d9c3c6
muslu <musluoglu.mert10@gmail.com>
2025-04-03 17:23:19 +0300

    Update RunAction.hh

--- geant4_2025/RunAction.hh
+++ geant4_2025/RunAction.hh
@@ -1,11 +1,10 @@
-#ifndef RunAction_h
-#define RunAction_h 1
+#ifndef RUN_ACTION_HH
+#define RUN_ACTION_HH
 
 #include "G4UserRunAction.hh"
 #include "globals.hh"
 
-class RunAction : public G4UserRunAction
-{
+class RunAction : public G4UserRunAction {
 public:
     RunAction();
     virtual ~RunAction();
