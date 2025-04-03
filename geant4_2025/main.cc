e6d4d4fcfa30bde514fba863433869b59aef33b6
muslu <musluoglu.mert10@gmail.com>
2025-04-03 21:02:38 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -1,36 +1,45 @@
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+#include "G4SystemOfUnits.hh"
+
 #include "PhysicsList.hh"
 #include "DetectorConstruction.hh"
 #include "PrimaryGeneratorAction.hh"
 #include "RunAction.hh"
 #include "EventAction.hh"
 
-int main() {
-  // 1. RunManager oluştur
-  G4RunManager* runManager = new G4RunManager();
+int main(int argc, char** argv) {
+    // 1. RunManager oluştur
+    G4RunManager* runManager = new G4RunManager();
 
-  // 2. Zorunlu başlatma sırası
-  runManager->SetUserInitialization(new PhysicsList());
-  
-  DetectorConstruction* detector = new DetectorConstruction();
-  detector->SetMaterial("G4_W");  // Tungsten varsayılan
-  detector->SetDimensions(10*cm, 10*cm);  // 10cm çap/yükseklik
-  runManager->SetUserInitialization(detector);
+    // 2. Zorunlu başlatma sırası
+    runManager->SetUserInitialization(new DetectorConstruction());
+    runManager->SetUserInitialization(new PhysicsList());
 
-  // 3. Aksiyonlar
-  runManager->SetUserAction(new PrimaryGeneratorAction());
-  runManager->SetUserAction(new RunAction());
-  runManager->SetUserAction(new EventAction());
+    // 3. Aksiyonlar
+    runManager->SetUserAction(new PrimaryGeneratorAction());
+    runManager->SetUserAction(new RunAction());
+    runManager->SetUserAction(new EventAction());
 
-  // 4. Görselleştirme (OGL)
-  G4UImanager* UI = G4UImanager::GetUIpointer();
-  UI->ApplyCommand("/control/execute macros/init_vis.mac");  // 3D görsel
-
-  // 5. Simülasyonu başlat
-  runManager->Initialize();
-  runManager->BeamOn(1000);  // 1000 olay
-
-  delete runManager;
-  return 0;
+    // 4. Görselleştirme
+    G4VisManager* visManager = new G4VisExecutive();
+    visManager->Initialize();
+    
+    // 5. UI (GUI veya batch mod)
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+    G4UImanager* UI = G4UImanager::GetUIpointer();
+    
+    // Otomatik 3D görünüm
+    UI->ApplyCommand("/control/execute macros/init_vis.mac");
+    UI->ApplyCommand("/run/initialize");
+    
+    // 6. Simülasyonu başlat
+    ui->SessionStart();
+    
+    delete ui;
+    delete visManager;
+    delete runManager;
+    return 0;
 }
