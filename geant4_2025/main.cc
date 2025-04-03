530f8484da9754a78d96c480d88b3814d1fc9e53
muslu <musluoglu.mert10@gmail.com>
2025-04-03 17:24:22 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -2,14 +2,22 @@
 #include "DetectorConstruction.hh"
 #include "PrimaryGeneratorAction.hh"
 #include "RunAction.hh"
+#include "EventAction.hh"
 
 int main() {
-  G4RunManager* runManager = new G4RunManager();
-  runManager->SetUserInitialization(new DetectorConstruction());
-  runManager->SetUserAction(new PrimaryGeneratorAction());
-  runManager->SetUserAction(new RunAction());
-  runManager->Initialize();
-  runManager->BeamOn(10000); // 10k proton atışı
-  delete runManager;
-  return 0;
+    G4RunManager* runManager = new G4RunManager();
+    
+    DetectorConstruction* detector = new DetectorConstruction();
+    detector->SetMaterial("G4_W");  // Tungsten
+    detector->SetDimensions(10*cm, 10*cm);
+    runManager->SetUserInitialization(detector);
+
+    runManager->SetUserAction(new PrimaryGeneratorAction());
+    runManager->SetUserAction(new RunAction());
+    runManager->SetUserAction(new EventAction());
+
+    runManager->Initialize();
+    runManager->BeamOn(10000);
+    delete runManager;
+    return 0;
 }
