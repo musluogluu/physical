e10606f14c809db3b96fced1825d8ccf1d96f617
muslu <musluoglu.mert10@gmail.com>
2025-04-03 19:04:16 +0300

    Update RunAction.cc

--- geant4_2025/RunAction.cc
+++ geant4_2025/RunAction.cc
@@ -1,23 +1,14 @@
 #include "RunAction.hh"
 #include "G4Run.hh"
-#include "G4AnalysisManager.hh"
-#include "G4SystemOfUnits.hh"
 
-RunAction::RunAction() {}
+RunAction::RunAction() {}  // Constructor implementasyonu EKLENDİ
 
-RunAction::~RunAction() {}  // Düzeltme: Destructor implemente edildi
+RunAction::~RunAction() {} // Destructor implementasyonu EKLENDİ
 
-void RunAction::BeginOfRunAction(const G4Run*) {
-    G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
-    analysisManager->OpenFile("neutron_data");
-    analysisManager->CreateNtuple("NeutronData", "Data");
-    analysisManager->CreateNtupleDColumn("Energy");
-    analysisManager->CreateNtupleIColumn("Count");
-    analysisManager->FinishNtuple();
+void RunAction::BeginOfRunAction(const G4Run* run) {
+    // Implementasyon buraya
 }
 
-void RunAction::EndOfRunAction(const G4Run*) {
-    G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
-    analysisManager->Write();
-    analysisManager->CloseFile();
+void RunAction::EndOfRunAction(const G4Run* run) {
+    // Implementasyon buraya
 }
