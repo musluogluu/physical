4860de5b54dca10f1d2d6dc56269f43437c23ba9
muslu <musluoglu.mert10@gmail.com>
2025-04-03 18:31:34 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -1,4 +1,5 @@
 #include "G4RunManager.hh"
+#include "PhysicsList.hh"  // Yeni ekle
 #include "DetectorConstruction.hh"
 #include "PrimaryGeneratorAction.hh"
 #include "RunAction.hh"
@@ -8,17 +9,22 @@
 int main() {
     G4RunManager* runManager = new G4RunManager();
     
+    // 1. ÖNCE PhysicsList
+    runManager->SetUserInitialization(new PhysicsList());
+    
+    // 2. SONRA Detector
     DetectorConstruction* detector = new DetectorConstruction();
-    detector->SetMaterial("G4_W");  // Tungsten
+    detector->SetMaterial("G4_W");
     detector->SetDimensions(10*cm, 10*cm);
     runManager->SetUserInitialization(detector);
 
+    // 3. Diğer aksiyonlar
     runManager->SetUserAction(new PrimaryGeneratorAction());
     runManager->SetUserAction(new RunAction());
     runManager->SetUserAction(new EventAction());
 
     runManager->Initialize();
-    runManager->BeamOn(10000);  // 10k proton atışı
+    runManager->BeamOn(10000);
     delete runManager;
     return 0;
 }
