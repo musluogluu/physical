720803f656ca9bce82b4b834ad3368824daff575
muslu <musluoglu.mert10@gmail.com>
2025-04-03 21:34:19 +0300

    Update PhysicsList.cc

--- geant4_2025/PhysicsList.cc
+++ geant4_2025/PhysicsList.cc
@@ -1,21 +1,9 @@
 #include "PhysicsList.hh"
-#include "FTFP_BERT.hh"  // Doğrudan ekleyin
-#include "G4EmStandardPhysics_option4.hh"
-#include "G4DecayPhysics.hh"
-#include "G4HadronicParameters.hh"
+#include "FTFP_BERT.hh"  // Tüm temel fizik süreçlerini içerir
 
 PhysicsList::PhysicsList() {
-    // 1. Temel süreçler
-    RegisterPhysics(new G4DecayPhysics());
-    
-    // 2. EM süreçler
-    RegisterPhysics(new G4EmStandardPhysics_option4());
-    
-    // 3. Hadronik fizik (DÜZELTME BURADA)
-    RegisterPhysics(new FTFP_BERT());  // Direkt kayıt
-    
-    // 4. Enerji aralığı ayarı
-    G4HadronicParameters::Instance()->SetMaxEnergy(1.2*GeV);
+    // TEK SATIRDA FİZİK LİSTESİ
+    RegisterPhysics(new FTFP_BERT());  // Hem EM hem hadronik süreçler
 }
 
 PhysicsList::~PhysicsList() {}
