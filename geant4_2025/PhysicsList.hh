a393d9df08684153fefbee133a13a51086c31c77
muslu <musluoglu.mert10@gmail.com>
2025-04-03 18:42:43 +0300

    Update PhysicsList.hh

--- geant4_2025/PhysicsList.hh
+++ geant4_2025/PhysicsList.hh
@@ -1,1 +1,12 @@
+#ifndef PHYSICSLIST_HH
+#define PHYSICSLIST_HH
 
+#include "G4VModularPhysicsList.hh"
+
+class PhysicsList : public G4VModularPhysicsList {
+public:
+    PhysicsList();
+    virtual ~PhysicsList();
+};
+
+#endif
