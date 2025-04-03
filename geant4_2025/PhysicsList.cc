3fa93b82dc00c917a595edda0d7536baaf97e8c1
muslu <musluoglu.mert10@gmail.com>
2025-04-03 21:03:03 +0300

    Update PhysicsList.cc

--- geant4_2025/PhysicsList.cc
+++ geant4_2025/PhysicsList.cc
@@ -1,21 +1,24 @@
 #include "PhysicsList.hh"
-#include "G4PhysListFactory.hh"  // Fizik listesi fabrikası için
+#include "G4PhysListFactory.hh"
 #include "G4EmStandardPhysics_option4.hh"
 #include "G4DecayPhysics.hh"
 #include "G4RadioactiveDecayPhysics.hh"
 
 PhysicsList::PhysicsList() {
-    // 1. Temel bozunum fizikleri
+    // 1. Temel süreçler
     RegisterPhysics(new G4DecayPhysics());
-
-    // 2. EM süreçler (1.5 GeV için optimize edilmiş)
+    
+    // 2. EM süreçler (1.5 GeV için optimize)
     RegisterPhysics(new G4EmStandardPhysics_option4());
-
-    // 3. Hadronik fizik listesi (DÜZELTME BURADA)
+    
+    // 3. Hadronik fizik (FTFP_BERT)
     G4PhysListFactory factory;
-    RegisterPhysics(factory.GetReferencePhysList("FTFP_BERT"));
-
-    // 4. Radyoaktif bozunum (opsiyonel)
+    auto physList = factory.GetReferencePhysList("FTFP_BERT");
+    for(auto physics : *physList->GetPhysicsVector()) {
+        RegisterPhysics(physics);
+    }
+    
+    // 4. Radyoaktif bozunum
     RegisterPhysics(new G4RadioactiveDecayPhysics());
 }
 
