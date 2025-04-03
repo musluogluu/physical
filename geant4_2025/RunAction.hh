55771c11bdd9b4e538ef37b6493386fee205f7bc
muslu <musluoglu.mert10@gmail.com>
2025-04-03 19:04:03 +0300

    Update RunAction.hh

--- geant4_2025/RunAction.hh
+++ geant4_2025/RunAction.hh
@@ -2,15 +2,14 @@
 #define RUN_ACTION_HH
 
 #include "G4UserRunAction.hh"
-#include "globals.hh"
 
 class RunAction : public G4UserRunAction {
 public:
     RunAction();
-    virtual ~RunAction();  // Düzeltme: Virtual destructor eklendi
+    virtual ~RunAction();  // Virtual destructor EKLENDİ
     
-    virtual void BeginOfRunAction(const G4Run*);
-    virtual void EndOfRunAction(const G4Run*);
+    virtual void BeginOfRunAction(const G4Run* run) override;
+    virtual void EndOfRunAction(const G4Run* run) override;
 };
 
 #endif
