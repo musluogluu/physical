5a4db86ecb344a2e3853cc112d5d4c60f93b68dc
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:29:28 +0300

    Update DetectorConstruction.cc

--- geant4_2025/DetectorConstruction.cc
+++ geant4_2025/DetectorConstruction.cc
@@ -3,26 +3,13 @@
 #include "G4Tubs.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
-#include "G4SystemOfUnits.hh"
 
-DetectorConstruction::DetectorConstruction()
-: fDiameter(15*cm), fHeight(60*cm) {
+DetectorConstruction::DetectorConstruction() {
     fMaterial = G4NistManager::Instance()->FindOrBuildMaterial("G4_W");
 }
 
 G4VPhysicalVolume* DetectorConstruction::Construct() {
-    // Silindir hedef
     G4Tubs* solidTarget = new G4Tubs("Target", 0, fDiameter/2, fHeight/2, 0, 360*deg);
     G4LogicalVolume* logicTarget = new G4LogicalVolume(solidTarget, fMaterial, "Target");
-    
-    return new G4PVPlacement(0, G4ThreeVector(), logicTarget, "Target", 0, false, 0);
-}
-
-void DetectorConstruction::SetMaterial(const G4String& materialName) {
-    fMaterial = G4NistManager::Instance()->FindOrBuildMaterial(materialName);
-}
-
-void DetectorConstruction::SetDimensions(G4double diameter, G4double height) {
-    fDiameter = diameter;
-    fHeight = height;
+    return new G4PVPlacement(0, G4ThreeVector(), logicTarget, "Target", nullptr, false, 0);
 }
