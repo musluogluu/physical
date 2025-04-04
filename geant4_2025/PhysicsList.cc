02f5dcdd2f011d1a43fb5f6a59c081df221d155b
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:01:40 +0300

    Update PhysicsList.cc

--- geant4_2025/PhysicsList.cc
+++ geant4_2025/PhysicsList.cc
@@ -1,9 +1,26 @@
 #include "PhysicsList.hh"
-#include "FTFP_BERT.hh"  // Tüm temel fizik süreçlerini içerir
+#include "G4VModularPhysicsList.hh"
+#include "G4VPhysicsConstructor.hh"
+#include "G4PhysListFactory.hh"
+#include "G4ParticleTypes.hh"
+#include "FTFP_BERT.hh"
 
-PhysicsList::PhysicsList() {
-    // TEK SATIRDA FİZİK LİSTESİ
-    RegisterPhysics(new FTFP_BERT());  // Hem EM hem hadronik süreçler
+PhysicsList::PhysicsList() : G4VModularPhysicsList() {
+    // Geant4 fizik listesi fabrikasını kullanarak bir fizik listesi oluştur
+    G4PhysListFactory factory;
+    G4VModularPhysicsList* physList = factory.GetReferencePhysList("FTFP_BERT");
+
+    if (physList) {
+        for (G4int i = 0; i < physList->GetPhysicsVector()->size(); i++) {
+            RegisterPhysics((*physList->GetPhysicsVector())[i]);
+        }
+    } else {
+        G4cerr << "Error: Could not create physics list!" << G4endl;
+    }
 }
 
 PhysicsList::~PhysicsList() {}
+
+void PhysicsList::SetCuts() {
+    SetCutsWithDefault();
+}
