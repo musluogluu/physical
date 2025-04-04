d3093f64a526c44c25ea92ded34fd600f8a29ed0
muslu <musluoglu.mert10@gmail.com>
2025-04-04 12:04:21 +0300

    Update DetectorConstruction.hh

--- geant4_2025/DetectorConstruction.hh
+++ geant4_2025/DetectorConstruction.hh
@@ -5,17 +5,21 @@
 #include "G4Material.hh"
 #include "G4SystemOfUnits.hh"
 
+class EventAction; // Forward declaration eklendi
+
 class DetectorConstruction : public G4VUserDetectorConstruction {
 public:
     DetectorConstruction();
     virtual ~DetectorConstruction() = default;
     
     virtual G4VPhysicalVolume* Construct() override;
+    void SetEventAction(EventAction* eventAction); // Yeni eklendi
     
 private:
     G4double fDiameter = 15*cm;
     G4double fHeight = 60*cm;
     G4Material* fMaterial = nullptr;
+    EventAction* fEventAction = nullptr; // Yeni eklendi
 };
 
 #endif
