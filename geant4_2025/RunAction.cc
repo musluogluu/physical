dede1a23d3ff48f501c4381b8aa065f5065e567c
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:30:44 +0300

    Update RunAction.cc

--- geant4_2025/RunAction.cc
+++ geant4_2025/RunAction.cc
@@ -1,20 +1,11 @@
 #include "RunAction.hh"
-#include "G4AnalysisManager.hh"
 
-RunAction::RunAction() {
-  // Analiz dosyasını oluştur
-  G4AnalysisManager* analysis = G4AnalysisManager::Instance();
-  analysis->CreateNtuple("NeutronData", "Nötron Bilgileri");
-  analysis->CreateNtupleIColumn("NeutronCount");
-  analysis->CreateNtupleDColumn("MaxNeutronEnergy");
-  analysis->FinishNtuple();
-}
+RunAction::RunAction() {}
 
 void RunAction::BeginOfRunAction(const G4Run*) {
-  G4AnalysisManager::Instance()->OpenFile("output.root");
+    // Implement if needed
 }
 
 void RunAction::EndOfRunAction(const G4Run*) {
-  G4AnalysisManager::Instance()->Write();
-  G4AnalysisManager::Instance()->CloseFile();
+    // Implement if needed
 }
