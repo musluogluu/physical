3590480778bbbb59297a4c2d0cc49d8b3f6a7061
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:57:47 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -10,24 +10,52 @@
 #include "EventAction.hh"
 
 int main(int argc, char** argv) {
+    // 1. RunManager oluşturma
     G4RunManager* runManager = new G4RunManager();
     
-    runManager->SetUserInitialization(new DetectorConstruction());
+    // 2. Detector Construction - Nötron dedektörlü versiyon
+    DetectorConstruction* detector = new DetectorConstruction();
+    runManager->SetUserInitialization(detector);
+    
+    // 3. Fizik listesi
     runManager->SetUserInitialization(new PhysicsList());
     
-    runManager->SetUserAction(new PrimaryGeneratorAction());
-    runManager->SetUserAction(new RunAction());
-    runManager->SetUserAction(new EventAction());
+    // 4. Aksiyon sınıflarını oluştur ve bağla
+    RunAction* runAction = new RunAction();
+    EventAction* eventAction = new EventAction(runAction);
     
+    // Dedektöre event action'ı bağla
+    detector->SetEventAction(eventAction);
+    
+    // 5. Kullanıcı aksiyonlarını ayarla
+    runManager->SetUserAction(new PrimaryGeneratorAction());
+    runManager->SetUserAction(runAction);
+    runManager->SetUserAction(eventAction);
+    
+    // 6. Görselleştirme ayarları
     G4VisManager* visManager = new G4VisExecutive();
     visManager->Initialize();
     
+    // 7. UI yöneticisi
     G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-    G4UImanager::GetUIpointer()->ApplyCommand("/control/execute macros/init_vis.mac");
-    ui->SessionStart();
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
     
+    // 8. Görselleştirme komutları
+    UImanager->ApplyCommand("/control/execute macros/init_vis.mac");
+    
+    // 9. Batch mod veya interaktif mod
+    if(argc == 1) { // Interaktif mod
+        ui->SessionStart();
+    } else { // Batch mod
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command + fileName);
+    }
+    
+    // 10. Temizlik
     delete ui;
     delete visManager;
     delete runManager;
+    
     return 0;
 }
