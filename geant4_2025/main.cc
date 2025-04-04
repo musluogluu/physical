6c5d307e7307f629b74ed30f32a0dfabd86ecd03
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:28:39 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -10,30 +10,22 @@
 #include "EventAction.hh"
 
 int main(int argc, char** argv) {
-    // 1. RunManager oluştur
     G4RunManager* runManager = new G4RunManager();
-
-    // 2. Zorunlu başlatma
+    
     runManager->SetUserInitialization(new DetectorConstruction());
     runManager->SetUserInitialization(new PhysicsList());
-
-    // 3. Aksiyonlar
+    
     runManager->SetUserAction(new PrimaryGeneratorAction());
     runManager->SetUserAction(new RunAction());
     runManager->SetUserAction(new EventAction());
-
-    // 4. Görselleştirme
+    
     G4VisManager* visManager = new G4VisExecutive();
     visManager->Initialize();
-
-    // 5. UI
+    
     G4UIExecutive* ui = new G4UIExecutive(argc, argv);
     G4UImanager::GetUIpointer()->ApplyCommand("/control/execute macros/init_vis.mac");
-    
-    // 6. Simülasyon
-    runManager->Initialize();
     ui->SessionStart();
-
+    
     delete ui;
     delete visManager;
     delete runManager;
