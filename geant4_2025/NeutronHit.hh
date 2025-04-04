f87456d5158fe8ee7bedff0a444652f1968d3249
muslu <musluoglu.mert10@gmail.com>
2025-04-04 21:18:35 +0300

    Update NeutronHit.hh

--- geant4_2025/NeutronHit.hh
+++ geant4_2025/NeutronHit.hh
@@ -1,9 +1,8 @@
-#ifndef NEUTRONHIT_HH
-#define NEUTRONHIT_HH
+#ifndef NeutronHit_h
+#define NeutronHit_h 1
 
 #include "G4VHit.hh"
 #include "G4THitsCollection.hh"
-#include "G4Allocator.hh"
 #include "G4ThreeVector.hh"
 
 class NeutronHit : public G4VHit {
@@ -11,22 +10,22 @@ public:
     NeutronHit();
     virtual ~NeutronHit();
     
-    void* operator new(size_t);
-    void operator delete(void* aHit);
-    
+    // Set/Get metodları
     void SetEnergy(G4double e) { fEnergy = e; }
     G4double GetEnergy() const { return fEnergy; }
     
-    void SetMomentumDirection(G4ThreeVector dir) { fMomentumDir = dir; }
-    G4ThreeVector GetMomentumDirection() const { return fMomentumDir; }
+    void SetPosition(G4ThreeVector pos) { fPos = pos; }
+    G4ThreeVector GetPosition() const { return fPos; }
     
+    void SetMomentumDirection(G4ThreeVector dir) { fDir = dir; }
+    G4ThreeVector GetMomentumDirection() const { return fDir; }
+
 private:
     G4double fEnergy;
-    G4ThreeVector fMomentumDir;
+    G4ThreeVector fPos;
+    G4ThreeVector fDir;
 };
 
 typedef G4THitsCollection<NeutronHit> NeutronHitsCollection;
-
-extern G4ThreadLocal G4Allocator<NeutronHit>* NeutronHitAllocator;
 
 #endif
