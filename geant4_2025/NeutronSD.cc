8f01ff3da28a5cb29946bc233b52a1697ecb4dbb
muslu <musluoglu.mert10@gmail.com>
2025-04-04 12:05:03 +0300

    Update NeutronSD.cc

--- geant4_2025/NeutronSD.cc
+++ geant4_2025/NeutronSD.cc
@@ -1,10 +1,13 @@
 #include "NeutronSD.hh"
+#include "EventAction.hh"
 #include "G4Step.hh"
 #include "G4Track.hh"
 #include "G4Neutron.hh"
 
-NeutronSD::NeutronSD(const G4String& name) 
-    : G4VSensitiveDetector(name), fEventAction(nullptr) {}
+NeutronSD::NeutronSD(const G4String& name, EventAction* eventAction)
+    : G4VSensitiveDetector(name), fEventAction(eventAction) {
+    collectionName.insert("NeutronHitsCollection");
+}
 
 void NeutronSD::Initialize(G4HCofThisEvent*) {}
 
@@ -12,9 +15,7 @@ G4bool NeutronSD::ProcessHits(G4Step* step, G4TouchableHistory*) {
     G4Track* track = step->GetTrack();
     if(track->GetDefinition() == G4Neutron::NeutronDefinition()) {
         G4double energy = track->GetKineticEnergy();
-        if(fEventAction) {
-            fEventAction->AddNeutron(energy);
-        }
+        fEventAction->AddNeutron(energy);
     }
     return true;
 }
