776c652bcbdb4bb0b865d8f9f34fd5584d14e2f4
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:30:57 +0300

    Update EventAction.hh

--- geant4_2025/EventAction.hh
+++ geant4_2025/EventAction.hh
@@ -6,7 +6,7 @@
 class EventAction : public G4UserEventAction {
 public:
     EventAction();
-    virtual ~EventAction();  // virtual ekleyin
+    virtual ~EventAction() = default;
     
     virtual void BeginOfEventAction(const G4Event*) override;
     virtual void EndOfEventAction(const G4Event*) override;
