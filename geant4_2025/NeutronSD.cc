c4a78a6493ac99532c5532b76bfd7996c59c1067
muslu <musluoglu.mert10@gmail.com>
2025-04-04 21:19:55 +0300

    Update NeutronSD.cc

--- geant4_2025/NeutronSD.cc
+++ geant4_2025/NeutronSD.cc
@@ -1,43 +1,32 @@
 #include "NeutronSD.hh"
+#include "G4Neutron.hh"
 #include "G4Step.hh"
 #include "G4Track.hh"
-#include "G4Neutron.hh"
+#include "G4HCofThisEvent.hh"
 #include "G4SDManager.hh"
-#include "G4SystemOfUnits.hh"
 
-NeutronSD::NeutronSD(const G4String& name)
-    : G4VSensitiveDetector(name) {
+NeutronSD::NeutronSD(const G4String& name) 
+: G4VSensitiveDetector(name), fHitsCollection(nullptr), fHCID(-1) {
     collectionName.insert("NeutronHitsCollection");
 }
 
 void NeutronSD::Initialize(G4HCofThisEvent* hce) {
-    fHitsCollection = new NeutronHitsCollection(
-        SensitiveDetectorName, 
-        collectionName[0]
-    );
-    
-    if(hce) {
-        G4int hcID = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
-        hce->AddHitsCollection(hcID, fHitsCollection);
-    }
+    fHitsCollection = new NeutronHitsCollection(SensitiveDetectorName, collectionName[0]);
+    if(fHCID < 0) fHCID = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
+    hce->AddHitsCollection(fHCID, fHitsCollection);
 }
 
 G4bool NeutronSD::ProcessHits(G4Step* step, G4TouchableHistory*) {
     G4Track* track = step->GetTrack();
     
-    // Nötron kontrolü ve veri yazdırma
-    if(track->GetDefinition() == G4Neutron::NeutronDefinition()) {
-        G4cout << "NEUTRON_DATA: "
-               << "Angle=" << track->GetMomentumDirection().theta()/deg << "deg "
-               << "Energy=" << track->GetKineticEnergy()/MeV << "MeV "
-               << "Pos=" << track->GetPosition()/mm << "mm\n";
-    }
-    
-    // Mevcut hit işleme kodu (değiştirme)
+    // Sadece nötronları kaydet
     if(track->GetDefinition() != G4Neutron::NeutronDefinition()) return false;
+    
     NeutronHit* hit = new NeutronHit();
     hit->SetEnergy(track->GetKineticEnergy());
+    hit->SetPosition(track->GetPosition());
     hit->SetMomentumDirection(track->GetMomentumDirection());
+    
     fHitsCollection->insert(hit);
     return true;
 }
