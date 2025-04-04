3f18d9d052e9bed6030a87abbce99562aaccf39d
muslu <musluoglu.mert10@gmail.com>
2025-04-04 18:32:18 +0300

    Update NeutronSD.cc

--- geant4_2025/NeutronSD.cc
+++ geant4_2025/NeutronSD.cc
@@ -1,21 +1,34 @@
+// NeutronSD.cc
 #include "NeutronSD.hh"
-#include "EventAction.hh"
 #include "G4Step.hh"
 #include "G4Track.hh"
 #include "G4Neutron.hh"
+#include "G4SDManager.hh"
+#include "G4SystemOfUnits.hh"
 
-NeutronSD::NeutronSD(const G4String& name, EventAction* eventAction)
-    : G4VSensitiveDetector(name), fEventAction(eventAction) {
+NeutronSD::NeutronSD(const G4String& name)
+    : G4VSensitiveDetector(name) {
     collectionName.insert("NeutronHitsCollection");
 }
 
-void NeutronSD::Initialize(G4HCofThisEvent*) {}
+void NeutronSD::Initialize(G4HCofThisEvent* hce) {
+    fHitsCollection = new NeutronHitsCollection(SensitiveDetectorName, collectionName[0]);
+    if(hce) {
+        hce->AddHitsCollection(
+            G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]),
+            fHitsCollection
+        );
+    }
+}
 
 G4bool NeutronSD::ProcessHits(G4Step* step, G4TouchableHistory*) {
     G4Track* track = step->GetTrack();
-    if(track->GetDefinition() == G4Neutron::NeutronDefinition()) {
-        G4double energy = track->GetKineticEnergy();
-        fEventAction->AddNeutron(energy);
-    }
+    if(track->GetDefinition() != G4Neutron::NeutronDefinition()) return false;
+
+    NeutronHit* hit = new NeutronHit();
+    hit->SetEnergy(track->GetKineticEnergy());
+    hit->SetMomentumDirection(track->GetMomentumDirection());
+    fHitsCollection->insert(hit);
+    
     return true;
 }
