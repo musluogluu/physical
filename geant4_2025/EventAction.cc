b46855fe0294a7c645af0d34a8ccab97827b072b
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:34:16 +0300

    Update EventAction.cc

--- geant4_2025/EventAction.cc
+++ geant4_2025/EventAction.cc
@@ -1,32 +1,11 @@
 #include "EventAction.hh"
-#include "G4Event.hh"
-#include "G4AnalysisManager.hh"
-#include "G4Neutron.hh"
 
 EventAction::EventAction() {}
 
-void EventAction::BeginOfEventAction(const G4Event*) {}
+void EventAction::BeginOfEventAction(const G4Event*) {
+    // Implement if needed
+}
 
-void EventAction::EndOfEventAction(const G4Event* event) {
-  G4AnalysisManager* analysis = G4AnalysisManager::Instance();
-  
-  // Tüm parçacıkları tarayarak nötronları say
-  G4int nNeutrons = 0;
-  G4double maxNeutronEnergy = 0;
-
-  G4TrajectoryContainer* trajectories = event->GetTrajectoryContainer();
-  if (trajectories) {
-    for (size_t i=0; i<trajectories->size(); ++i) {
-      if ((*trajectories)[i]->GetParticleName() == "neutron") {
-        nNeutrons++;
-        G4double e = (*trajectories)[i]->GetInitialMomentum().mag();
-        if (e > maxNeutronEnergy) maxNeutronEnergy = e;
-      }
-    }
-  }
-
-  // Verileri kaydet
-  analysis->FillNtupleIColumn(0, nNeutrons);
-  analysis->FillNtupleDColumn(1, maxNeutronEnergy);
-  analysis->AddNtupleRow();
+void EventAction::EndOfEventAction(const G4Event*) {
+    // Implement if needed
 }
