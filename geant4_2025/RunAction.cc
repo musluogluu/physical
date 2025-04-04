5c1c739f708bf0751a108b12145c410bdab13084
muslu <musluoglu.mert10@gmail.com>
2025-04-04 18:30:55 +0300

    Update RunAction.cc

--- geant4_2025/RunAction.cc
+++ geant4_2025/RunAction.cc
@@ -1,39 +1,48 @@
+// RunAction.cc
 #include "RunAction.hh"
 #include "G4Run.hh"
-#include "G4UnitsTable.hh"
-#include "G4SystemOfUnits.hh"
 #include <fstream>
 
-RunAction::RunAction() : fNeutronCount(0) {
-    // Enerji spektrumu başlatma
-    for(int i=0; i<=1000; i+=10) {
-        fEnergySpectrum[i] = 0;
-    }
+RunAction::RunAction() : totalNeutrons(0) {
+    for(int i=0; i<=180; i+=5) angleHist[i] = 0;
+    for(int i=0; i<=1000; i+=10) energyHist[i] = 0;
 }
 
-RunAction::~RunAction() {}  // AÇIKÇA TANIMLANMIŞ DESTRUCTOR
+RunAction::~RunAction() {}
 
-void RunAction::BeginOfRunAction(const G4Run* run) {
-    fNeutronCount.Reset();
-    // Ek başlangıç işlemleri buraya
+void RunAction::BeginOfRunAction(const G4Run*) {
+    totalNeutrons = 0;
 }
 
-void RunAction::EndOfRunAction(const G4Run* run) {
-    G4int totalNeutrons = fNeutronCount.GetValue();
+void RunAction::RecordNeutron(G4double energy, G4double angle) {
+    G4int angleBin = static_cast<G4int>(angle/deg)/5 * 5;
+    angleHist[angleBin]++;
     
-    G4cout << "\n--- Run Summary ---" << G4endl;
-    G4cout << "Total neutrons produced: " << totalNeutrons << G4endl;
+    G4int energyBin = static_cast<G4int>(energy/MeV)/10 * 10;
+    energyHist[energyBin]++;
     
-    std::ofstream outFile("neutron_spectrum.csv");
-    outFile << "Energy[MeV],Count\n";
-    for(const auto& bin : fEnergySpectrum) {
-        outFile << bin.first << "," << bin.second << "\n";
-    }
-    outFile.close();
+    totalNeutrons++;
 }
 
-void RunAction::AddNeutronEnergy(G4double energy) {
-    fNeutronCount += 1;
-    G4int energyBin = ((G4int)(energy/MeV))/10 * 10;
-    fEnergySpectrum[energyBin]++;
+void RunAction::EndOfRunAction(const G4Run*) {
+    std::ofstream out("neutron_data.csv");
+    out << "Angle[deg],Count,Energy[MeV],Count\n";
+    
+    auto angleIt = angleHist.begin();
+    auto energyIt = energyHist.begin();
+    
+    while(angleIt != angleHist.end() || energyIt != energyHist.end()) {
+        if(angleIt != angleHist.end()) {
+            out << angleIt->first << "," << angleIt->second << ",";
+            ++angleIt;
+        } else out << ",,";
+        
+        if(energyIt != energyHist.end()) {
+            out << energyIt->first << "," << energyIt->second;
+            ++energyIt;
+        }
+        out << "\n";
+    }
+    
+    out.close();
 }
