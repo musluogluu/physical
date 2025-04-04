a839bf9e888b1834902c0cf62b7c8f51ba0d49be
muslu <musluoglu.mert10@gmail.com>
2025-04-04 18:59:02 +0300

    Update NeutronHit.cc

--- geant4_2025/NeutronHit.cc
+++ geant4_2025/NeutronHit.cc
@@ -1,18 +1,23 @@
-// NeutronHit.cc
-#include "NeutronHit.hh"
 #include "G4SystemOfUnits.hh"
+#include "NeutronHit.hh"
 
 G4ThreadLocal G4Allocator<NeutronHit>* NeutronHitAllocator = nullptr;
 
-NeutronHit::NeutronHit() : fEnergy(0), fMomentumDir() {}
+NeutronHit::NeutronHit() 
+    : fEnergy(0), fMomentumDir() {}
+
 NeutronHit::~NeutronHit() {}
 
 void* NeutronHit::operator new(size_t) {
-    if(!NeutronHitAllocator)
+    if(!NeutronHitAllocator) {
         NeutronHitAllocator = new G4Allocator<NeutronHit>;
+    }
     return (void*)NeutronHitAllocator->MallocSingle();
 }
 
-void NeutronHit::operator delete(void* hit) {
-    NeutronHitAllocator->FreeSingle((NeutronHit*) hit);
+void NeutronHit::operator delete(void* aHit) {
+    if(!NeutronHitAllocator) {
+        NeutronHitAllocator = new G4Allocator<NeutronHit>;
+    }
+    NeutronHitAllocator->FreeSingle((NeutronHit*) aHit);
 }
