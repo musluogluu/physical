187cb53f9185a87b3d7ebf67f002c0e0b85591aa
muslu <musluoglu.mert10@gmail.com>
2025-04-04 18:30:30 +0300

    Update RunAction.hh

--- geant4_2025/RunAction.hh
+++ geant4_2025/RunAction.hh
@@ -1,23 +1,24 @@
+// RunAction.hh
 #ifndef RUN_ACTION_HH
 #define RUN_ACTION_HH
 
 #include "G4UserRunAction.hh"
-#include "G4Accumulable.hh"
 #include <map>
 
 class RunAction : public G4UserRunAction {
 public:
     RunAction();
-    virtual ~RunAction();  // DESTRUCTOR mutlaka virtual olmalı
+    virtual ~RunAction();
     
-    virtual void BeginOfRunAction(const G4Run* run) override;
-    virtual void EndOfRunAction(const G4Run* run) override;
+    virtual void BeginOfRunAction(const G4Run*) override;
+    virtual void EndOfRunAction(const G4Run*) override;
     
-    void AddNeutronEnergy(G4double energy);
+    void RecordNeutron(G4double energy, G4double angle);
     
 private:
-    G4Accumulable<G4int> fNeutronCount;
-    std::map<G4int, G4int> fEnergySpectrum;
+    std::map<G4int, G4int> angleHist; // 5°'lik bölmeler
+    std::map<G4int, G4int> energyHist; // 10 MeV'lik bölmeler
+    G4int totalNeutrons;
 };
 
 #endif
