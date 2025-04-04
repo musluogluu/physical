41dcc16c2fe1d686e9ae7d7c00f1ddd85cc8f562
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:52:54 +0300

    Update EventAction.hh

--- geant4_2025/EventAction.hh
+++ geant4_2025/EventAction.hh
@@ -2,14 +2,21 @@
 #define EVENT_ACTION_HH
 
 #include "G4UserEventAction.hh"
+#include "RunAction.hh"
 
 class EventAction : public G4UserEventAction {
 public:
-    EventAction();
+    EventAction(RunAction* runAction);
     virtual ~EventAction() = default;
     
     virtual void BeginOfEventAction(const G4Event*) override;
     virtual void EndOfEventAction(const G4Event*) override;
+    
+    void AddNeutron(G4double energy);
+    
+private:
+    RunAction* fRunAction;
+    std::vector<G4double> fNeutronEnergies;
 };
 
 #endif
