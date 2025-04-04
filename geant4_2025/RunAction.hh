14722d8a00326b2cdcd6fca719249e6cd71ec810
muslu <musluoglu.mert10@gmail.com>
2025-04-04 12:26:36 +0300

    Update RunAction.hh

--- geant4_2025/RunAction.hh
+++ geant4_2025/RunAction.hh
@@ -8,16 +8,16 @@
 class RunAction : public G4UserRunAction {
 public:
     RunAction();
-    virtual ~RunAction();
+    virtual ~RunAction();  // DESTRUCTOR mutlaka virtual olmalı
     
-    virtual void BeginOfRunAction(const G4Run*) override;
-    virtual void EndOfRunAction(const G4Run*) override;
+    virtual void BeginOfRunAction(const G4Run* run) override;
+    virtual void EndOfRunAction(const G4Run* run) override;
     
     void AddNeutronEnergy(G4double energy);
     
 private:
     G4Accumulable<G4int> fNeutronCount;
-    std::map<G4int, G4int> fEnergySpectrum; // MeV başına nötron sayısı
+    std::map<G4int, G4int> fEnergySpectrum;
 };
 
 #endif
