b70840c19daf82961b816abd71f554fa6bb93e74
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:29:51 +0300

    Update PhysicsList.cc

--- geant4_2025/PhysicsList.cc
+++ geant4_2025/PhysicsList.cc
@@ -1,26 +1,12 @@
 #include "PhysicsList.hh"
-#include "G4VModularPhysicsList.hh"
-#include "G4VPhysicsConstructor.hh"
 #include "G4PhysListFactory.hh"
-#include "G4ParticleTypes.hh"
-#include "FTFP_BERT.hh"
+#include "G4SystemOfUnits.hh"
 
-PhysicsList::PhysicsList() : G4VModularPhysicsList() {
-    // Geant4 fizik listesi fabrikasını kullanarak bir fizik listesi oluştur
+PhysicsList::PhysicsList() {
     G4PhysListFactory factory;
-    G4VModularPhysicsList* physList = factory.GetReferencePhysList("FTFP_BERT");
-
-    if (physList) {
-        for (G4int i = 0; i < physList->GetPhysicsVector()->size(); i++) {
-            RegisterPhysics((*physList->GetPhysicsVector())[i]);
-        }
-    } else {
-        G4cerr << "Error: Could not create physics list!" << G4endl;
+    auto physList = factory.GetReferencePhysList("FTFP_BERT");
+    
+    for(int i=0; i<physList->GetPhysicsLength(); i++) {
+        RegisterPhysics(const_cast<G4VPhysicsConstructor*>(physList->GetPhysics(i)));
     }
-}
-
-PhysicsList::~PhysicsList() {}
-
-void PhysicsList::SetCuts() {
-    SetCutsWithDefault();
 }
