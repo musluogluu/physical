b3f5880da50e990ff9a2bcd9d9e7d55cdd277751
muslu <musluoglu.mert10@gmail.com>
2025-04-04 21:19:28 +0300

    Update NeutronSD.hh

--- geant4_2025/NeutronSD.hh
+++ geant4_2025/NeutronSD.hh
@@ -1,6 +1,5 @@
-// NeutronSD.hh
-#ifndef NEUTRON_SD_HH
-#define NEUTRON_SD_HH
+#ifndef NeutronSD_h
+#define NeutronSD_h 1
 
 #include "G4VSensitiveDetector.hh"
 #include "NeutronHit.hh"
@@ -8,13 +7,14 @@
 class NeutronSD : public G4VSensitiveDetector {
 public:
     NeutronSD(const G4String& name);
-    virtual ~NeutronSD() = default;
+    virtual ~NeutronSD();
     
-    virtual void Initialize(G4HCofThisEvent* hitCollection) override;
-    virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history) override;
-    
+    virtual void Initialize(G4HCofThisEvent*);
+    virtual G4bool ProcessHits(G4Step*, G4TouchableHistory*);
+
 private:
     NeutronHitsCollection* fHitsCollection;
+    G4int fHCID;
 };
 
 #endif
