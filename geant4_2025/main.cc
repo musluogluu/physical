93f6b0957938b844e8d6f6718b27f0c9c08d1946
muslu <musluoglu.mert10@gmail.com>
2025-04-04 18:29:03 +0300

    Update main.cc

--- geant4_2025/main.cc
+++ geant4_2025/main.cc
@@ -12,23 +12,22 @@
 int main(int argc, char** argv) {
     G4RunManager* runManager = new G4RunManager();
     
-    auto detector = new DetectorConstruction();
-    runManager->SetUserInitialization(detector);
+    runManager->SetUserInitialization(new DetectorConstruction());
     runManager->SetUserInitialization(new PhysicsList());
     
-    auto runAction = new RunAction();
-    auto eventAction = new EventAction(runAction);
-    detector->SetEventAction(eventAction); // Kritik bağlantı eklendi
-    
+    RunAction* runAction = new RunAction();
     runManager->SetUserAction(new PrimaryGeneratorAction());
     runManager->SetUserAction(runAction);
-    runManager->SetUserAction(eventAction);
+    runManager->SetUserAction(new EventAction(runAction));
     
-    G4VisManager* visManager = new G4VisExecutive();
+    G4VisManager* visManager = new G4VisExecutive("quiet");
     visManager->Initialize();
     
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+    UImanager->ApplyCommand("/control/macroPath macros");
+    UImanager->ApplyCommand("/control/execute init_vis.mac");
+    
     G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-    G4UImanager::GetUIpointer()->ApplyCommand("/control/execute macros/init_vis.mac");
     ui->SessionStart();
     
     delete ui;
