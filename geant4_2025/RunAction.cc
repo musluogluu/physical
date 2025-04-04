06a4a35355dbdd04e9e1f499a12ef3f9c1a63971
muslu <musluoglu.mert10@gmail.com>
2025-04-04 12:27:08 +0300

    Update RunAction.cc

--- geant4_2025/RunAction.cc
+++ geant4_2025/RunAction.cc
@@ -5,14 +5,17 @@
 #include <fstream>
 
 RunAction::RunAction() : fNeutronCount(0) {
-    // Enerji spektrumu için 10 MeV'lik bölmeler
+    // Enerji spektrumu başlatma
     for(int i=0; i<=1000; i+=10) {
         fEnergySpectrum[i] = 0;
     }
 }
 
-void RunAction::BeginOfRunAction(const G4Run*) {
+RunAction::~RunAction() {}  // AÇIKÇA TANIMLANMIŞ DESTRUCTOR
+
+void RunAction::BeginOfRunAction(const G4Run* run) {
     fNeutronCount.Reset();
+    // Ek başlangıç işlemleri buraya
 }
 
 void RunAction::EndOfRunAction(const G4Run* run) {
@@ -21,7 +24,6 @@ void RunAction::EndOfRunAction(const G4Run* run) {
     G4cout << "\n--- Run Summary ---" << G4endl;
     G4cout << "Total neutrons produced: " << totalNeutrons << G4endl;
     
-    // Dosyaya yazma
     std::ofstream outFile("neutron_spectrum.csv");
     outFile << "Energy[MeV],Count\n";
     for(const auto& bin : fEnergySpectrum) {
@@ -32,6 +34,6 @@ void RunAction::EndOfRunAction(const G4Run* run) {
 
 void RunAction::AddNeutronEnergy(G4double energy) {
     fNeutronCount += 1;
-    G4int energyBin = ((G4int)(energy/MeV))/10 * 10; // 10 MeV'lik bölmeler
+    G4int energyBin = ((G4int)(energy/MeV))/10 * 10;
     fEnergySpectrum[energyBin]++;
 }
