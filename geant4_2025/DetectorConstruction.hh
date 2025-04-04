d6ee2220011afec0177e2f202bb6c9543abda75b
muslu <musluoglu.mert10@gmail.com>
2025-04-04 11:28:57 +0300

    Update DetectorConstruction.hh

--- geant4_2025/DetectorConstruction.hh
+++ geant4_2025/DetectorConstruction.hh
@@ -3,13 +3,19 @@
 
 #include "G4VUserDetectorConstruction.hh"
 #include "G4Material.hh"
+#include "G4SystemOfUnits.hh"
 
 class DetectorConstruction : public G4VUserDetectorConstruction {
 public:
     DetectorConstruction();
-    virtual ~DetectorConstruction();  // virtual ekleyin
+    virtual ~DetectorConstruction() = default;
     
-    virtual G4VPhysicalVolume* Construct() override;  // override ekleyin
+    virtual G4VPhysicalVolume* Construct() override;
+    
+private:
+    G4double fDiameter = 15*cm;
+    G4double fHeight = 60*cm;
+    G4Material* fMaterial = nullptr;
 };
 
 #endif
