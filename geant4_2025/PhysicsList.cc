bcbecded455328dfab417c362685974b67cb2375
muslu <musluoglu.mert10@gmail.com>
2025-04-04 12:22:46 +0300

    Update PhysicsList.cc

--- geant4_2025/PhysicsList.cc
+++ geant4_2025/PhysicsList.cc
@@ -1,12 +1,15 @@
 #include "PhysicsList.hh"
 #include "G4PhysListFactory.hh"
+#include "G4SystemOfUnits.hh"
 
 PhysicsList::PhysicsList() {
     G4PhysListFactory factory;
     auto physList = factory.GetReferencePhysList("FTFP_BERT");
     
-    // Alternatif yöntem - iterator kullanımı
-    for(size_t i=0; i<physList->size(); ++i) {
-        RegisterPhysics(const_cast<G4VPhysicsConstructor*>(physList->GetPhysics(i)));
+    // Geant4 v11.3.1 uyumlu kesin çözüm
+    G4int i = 0;
+    while(G4VPhysicsConstructor* physics = const_cast<G4VPhysicsConstructor*>(physList->GetPhysics(i++))) {
+        if(!physics) break;
+        RegisterPhysics(physics);
     }
 }
